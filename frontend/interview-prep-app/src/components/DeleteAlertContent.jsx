fedabb42e3851eeecc6dac836c6d9067917ccbf9
Anushka Mandal <anushka.mandal03@gmail.com>
2025-08-27 23:22:52 +0530

    Implemented load more and delete session

--- frontend/interview-prep-app/src/components/DeleteAlertContent.jsx
+++ frontend/interview-prep-app/src/components/DeleteAlertContent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DeleteAlertContent = ({content, onDelete}) => {
   return (
     <div className="p-5">
-      <p className="text-[14px]">{content}</p>
+      <p className="text-[14px]">{ content }</p>
 
       <div className="flex justify-end mt-6">
         <button
